60f99355f940843c2fc1295db8265695358a426c
Priyatham-sai-chand <priyathamsaichand@gmail.com>
2020-11-16 22:18:20 +0530

    pricing plan start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HomePage from "./components/HomePage";
 import Login from "./components/SignIn";
+import PricingPlan from "./components/PricingPlan";
 import LogInContainer from "./components/LogInContainer";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 class  App extends Component {
   render(){
     return (
       <div className="App">
+        <PricingPlan />
         <BrowserRouter>
           <Switch>
             <Route exact path="/" Component={HomePage} />
---
+++ src/components/PricingPlan.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './pricing-plan.css';
+
+class PricingPlan extends Component {
+
+    render(){
+        return(
+            <div className="body">
+                <section class='pricing-plan'>
+                    <div class='pricing-plan__header'>
+                        <h1 class='pricing-plan__title'>Basic Package</h1>
+                        <h2 class='pricing-plan__summary'>For those getting started</h2>
+
+                    </div>
+                    <div class='pricing-plan__desc'>
+                    </div>
+                </section>
+
+            </div>
+
+        )
+    }
+
+}
+export default PricingPlan;

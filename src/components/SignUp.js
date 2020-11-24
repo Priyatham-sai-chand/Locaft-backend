c45ae3c873d29f45990912617284e258ad71707a
Priyatham-sai-chand <priyathamsaichand@gmail.com>
2020-11-24 16:42:16 +0530

    sign up phone number

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -49,6 +49,10 @@ class SignUpForm extends Component {
                 <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
                 <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
               </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="phone">Phone number</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your Phone no. (+91)" name="phone" value={this.state.phone} onChange={this.handleChange} />
+              </div>
 
               <div className="FormField">
                 <label className="FormField__CheckboxLabel">

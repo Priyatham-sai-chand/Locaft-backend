afd61b2517e3332babe0649cd8ed9ce6c75043ca
Priyatham-sai-chand <priyathamsaichand@gmail.com>
2021-03-18 07:14:15 +0530

    seperate folders for client and server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const config =  require("config");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// set up express
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+// set up mongoose
+
+mongoose.connect(
+  config.get('mongoURI'),
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+  }
+);
+
+// set up routes
+
+app.use("/users", require("./routes/users"));
